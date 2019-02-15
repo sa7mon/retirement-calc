7eed8323761e887fbe3309039519968ec5aba80d
Dan Salmon <daniel.salmon@mnsu.edu>
2019-02-15 21:01:33 +0000

    Set initial values and add fields

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 /* global $ */
 
 var initial_age = 25;
-var initial_nest_egg = 2000;
+var initial_nest_egg = 5000;
 var initial_salary = 60000;
-var annual_contrib_percent = 0.08;
-var retire_age = 66;
+var annual_contrib_percent = 0.15;
+var retire_age = 62;
 var lame_age = 80;
 var annual_nest_egg_grow_percent = 0.04;
 var death_age = 100;
-var active_years_income = 60000;
-var lame_years_income = 40000
+var active_years_income = 50000;
+var lame_years_income = 25000
 
 function init() {
     // initial_age = $("#inp_initial_age").val();
@@ -17,6 +17,18 @@ function init() {
 }
 
 function calculate() {
+    /* Order of operations
+     * 
+     * Not retired
+     *  - Increase salary by percent
+     *  - Make contribution from salary
+     *  - Compound the interest
+     * 
+     * Retired
+     *  - Draw from nest egg
+     *  - Compound the interest
+     */
+    
     var current_nest_egg = initial_nest_egg;
     var current_salary = initial_salary;
     
