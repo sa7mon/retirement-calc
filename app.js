d6f5da93902c95a50e7f288fe492375f1904f260
Dan Salmon <daniel.salmon@mnsu.edu>
2019-02-15 17:53:02 +0000

    Add draws and salary changes after retirement

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ var initial_nest_egg = 2000;
 var initial_salary = 60000;
 var annual_contrib_percent = 0.08;
 var retire_age = 66;
+var lame_age = 80;
 var annual_nest_egg_grow_percent = 0.04;
 var death_age = 100;
+var active_years_income = 40000;
+var lame_years_income = 25000
 
 function init() {
     // initial_age = $("#inp_initial_age").val();
@@ -14,21 +17,35 @@ function init() {
 }
 
 function calculate() {
-    var principal = initial_nest_egg;
+    var current_nest_egg = initial_nest_egg;
     var current_salary = initial_salary;
     
-    for (var age = initial_age + 1; age < retire_age; age++) {
+    for (var age = initial_age + 1; age <= death_age; age++) {
+        var nest_egg_draw = 0;
+        
+        var is_retired = age >= retire_age ? true : false
+        
         // Yearly salary increase
-        current_salary = current_salary + (current_salary * 0.02)
+        current_salary = age >= retire_age ? 0 : current_salary + (current_salary * 0.02)
         
         // Annual contribution from salary at start of compounding period
-        principal += current_salary * annual_contrib_percent;
+        var contribution = age >= retire_age ? 0 : current_salary * annual_contrib_percent;
+        current_nest_egg += contribution;
+        
+        // Draw from nest egg if retired
+        if (age >= retire_age && age < lame_age) { // in active retirement years
+            nest_egg_draw = active_years_income;
+        } else if (age >= retire_age && age > lame_age) { // in lame years
+            nest_egg_draw = lame_years_income;
+        }
+        current_nest_egg -= nest_egg_draw
         
         // Compute interest and add to total
-        var interest = principal * annual_nest_egg_grow_percent;
-        principal += interest;
+        var interest = current_nest_egg * annual_nest_egg_grow_percent;
+        current_nest_egg += interest;
         
-        console.log("age:", age, "salary:", current_salary, "principal:", round(principal, 2), "interest:", round(interest, 2));
+        console.log("age:", age, "retired:", is_retired, "salary:", round(current_salary, 2), 
+        "contrib:", contribution, "draw:", nest_egg_draw, "interest:", round(interest, 2), "nest-egg:", round(current_nest_egg, 2));
     }
 }
 
---
+++ devnotes.md
@@ -0,0 +1,5 @@
+# Dev Notes
+
+## 2/15/19
+* Add the incrase in salary per year
+* Need to get some things clarified by Dr. V
